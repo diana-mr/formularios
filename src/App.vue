4368b94f023073d5278a8fe0b53336e3330840cd
diana-mr <dianamrom25@gmail.com>
2022-09-30 18:16:40 -0500

    formularios

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mt-3">
+  <Header />
+
+  <formulario />
+  </div>
+</template>
+
+<script>
+  import Header from "./components/Header.vue";
+  import Formulario from "./components/formulario.vue";
+
+  export default {
+    components: {
+      Header,
+      Formulario,
+    }
+  }
+</script>
---
+++ src/components/formulario.vue
@@ -0,0 +1,73 @@
+<template>
+  <form @submit.prevent="registrarProyecto">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Proyecto</label>
+      <input v-model.trim="proyecto"  type="text" class="form-control" required >
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Actividad</label>
+      <select v-model.trim="tipo" class="form-select">
+        <option selected>Selecciona una actididad</option>
+        <option>Aplicaciones web con Vue.js</option>
+        <option>BackEnd Services</option>
+        <option>Three</option>
+      </select>
+    </div>
+    <div class="form-check">
+      <input v-model.trim="urgente" class="form-check-input" type="checkbox">
+      <label class="form-check-label" for="flexRadioDefault1">
+        Urgente
+      </label>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Guardar</button>
+  </form>
+  <hr>
+  <div class="table-responsive">
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Proyecto</th>
+          <th>Tipo</th>
+          <th>Urgente</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Laravel app</td>
+          <td>Laravel</td>
+          <td class="bg-success">Si / No</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      proyecto: "",
+      tipo: "",
+      urgente: false,
+      proyectos: [],
+    }),
+    methods: {
+      registrarProyecto(){
+        const proyecto = {
+          proyecto: this.proyecto,
+          tipo: this.tipo,
+          urgente: this.urgente,
+        };
+        this.proyectos.push(proyecto)
+        console.log(this.proyectos);
+
+        this.proyecto = "";
+        this.tipo = "";
+        this.urgente = false;
+      },
+    },
+  };
+
+</script>
