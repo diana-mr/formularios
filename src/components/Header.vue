e3511bf661f0df649a74ee5dcae6ea99d01b8a09
diana-mr <dianamrom25@gmail.com>
2022-10-05 19:45:07 -0500

    formularios

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="row">
-        <div class="col-12-md-3">
+        <div class="col-12 col-md-3">
           <h1>Formularios</h1>
         </div>
-        <div class="col-12-md-6">
+        <div class="col-12 col-md-6">
           <img width="100" :src="url" :alt="msg">
         </div>
     </div>
--- src/components/formulario.vue
+++ src/components/formulario.vue
@@ -1,48 +1,66 @@
 <template>
-  <form @submit.prevent="registrarProyecto">
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Proyecto</label>
-      <input v-model.trim="proyecto"  type="text" class="form-control" required >
+  <div class="col-12 mb-4">
+    <h3 class="text-center">Progreso 0%</h3>
+    <div class="progress">
+      <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" 
+      role="progressbar"  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"
+      style="width: 50%">
     </div>
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Actividad</label>
-      <select v-model.trim="tipo" class="form-select">
-        <option selected>Selecciona una actididad</option>
-        <option>Aplicaciones web con Vue.js</option>
-        <option>BackEnd Services</option>
-        <option>Three</option>
-      </select>
     </div>
-    <div class="form-check">
-      <input v-model.trim="urgente" class="form-check-input" type="checkbox">
-      <label class="form-check-label" for="flexRadioDefault1">
-        Urgente
-      </label>
+  </div>
+  <div class="row">
+    <div class="col-12 col-md-4">
+      <form @submit.prevent="registrarProyecto">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Proyecto</label>
+          <input v-model.trim="proyecto"  type="text" class="form-control" required >
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Actividad</label>
+          <select v-model.trim="tipo" class="form-select">
+            <option selected>Selecciona una actididad</option>
+            <option>Aplicaciones web con Vue.js</option>
+            <option>BackEnd Services</option>
+            <option>App móvil con React</option>
+          </select>
+        </div>
+        <div class="form-check">
+          <input v-model.trim="urgente" class="form-check-input" type="checkbox">
+          <label class="form-check-label" for="flexRadioDefault1">
+            Urgente
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
     </div>
 
-    <button type="submit" class="btn btn-primary">Guardar</button>
-  </form>
-  <hr>
-  <div class="table-responsive">
-    <table class="table table-dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Proyecto</th>
-          <th>Tipo</th>
-          <th>Urgente</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(proyecto, clave) in proyectos" :key="clave">
-          <td>{{clave+1}}</td>
-          <td>{{proyecto.proyecto}}</td>
-          <td>{{proyecto.tipo}}</td>
-          <td class="bg-success" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{proyecto.urgente ? "Si" : "No"}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="col-12 col-md-8">
+      <h3>Total de proyectos: {{ numeroProyectos }}</h3>
+      <div class="table-responsive">
+        <table class="table table-dark table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Proyecto</th>
+              <th>Tipo</th>
+              <th>Urgente</th>
+              <th>Completado</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(proyecto, index) in proyectos" :key="index">
+              <td>{{index+1}}</td>
+              <td>{{proyecto.proyecto}}</td>
+              <td>{{proyecto.tipo}}</td>
+              <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{proyecto.urgente ? "Si" : "No"}}</td>
+              <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{proyecto.completado ? "Completado" : "Incompleto"}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    </div>
+    {{ porcentaje }}
 </template>
 
 <script>
@@ -52,6 +70,7 @@
       tipo: "",
       urgente: false,
       proyectos: [],
+      completado: false,
     }),
     methods: {
       registrarProyecto(){
@@ -60,13 +79,31 @@
           tipo: this.tipo,
           urgente: this.urgente,
         };
-        this.proyectos.push(proyecto)
-        console.log(this.proyectos);
+        this.proyectos.push(proyecto);
 
         this.proyecto = "";
         this.tipo = "";
         this.urgente = false;
       },
+      cambiarEstado (proyecto, campo){
+        //this.proyectos[id].urgente = !this.proyectos[id].urgente;
+        //console.log(proyecto, campo);
+        proyecto[campo] = ! proyecto[campo];
+      }
+    },
+    computed: {
+      numeroProyectos (){
+        return this.proyectos.length;
+      },
+      porcentaje (){
+        let completados = 0;
+
+        this.proyectos.map(proyecto => {
+          if(proyecto.completado) completados++;
+        });
+        return(completados * 100) / this.numeroProyectos;
+      }
+
     },
   };
 
